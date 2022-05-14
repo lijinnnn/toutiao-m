6683faca26f2fd2976692cc366addbbd232acad3
git_lijin <smalljz2022@163.com>
2022-05-14 08:59:23 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
-
+// 注册使用Vant组件库
+Vue.use(Vant)
 new Vue({
   router,
   store,
